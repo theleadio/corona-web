a1d5d7ba4cab1d401a6e0a6e0c549d2411423bc9
Dr. Lau Cher Han <drhanlau@DrLau-MacBook-Pro.local>
2020-01-26 10:41:46 +0800

    created news route

---
+++ routes/news.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql2');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  var conn = mysql.createConnection({
+      host : 'coronatracker.coehycitad7u.ap-southeast-1.rds.amazonaws.com',
+      user : 'corona',
+      password : 'corona0106',
+      database : 'coronatracker'
+  });
+
+  conn.connect();
+
+  conn.query('SELECT * FROM news', function(error, results){
+      if ( error ){
+          res.status(400).send('Error in database operation');
+      } else {
+          res.json(results);
+      }
+  });
+
+});
+
+module.exports = router;
