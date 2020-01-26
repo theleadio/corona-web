40637921d6a04fdab44d8f3db21c4b87cd16a0d6
Wei Seng Tan <tanweiseng13@gmail.com>
2020-01-27 01:46:26 +0800

    Remove deprecated trendingNews API

--- app.js
+++ app.js
@@ -28,7 +28,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/news', newsRouter);
-app.use('/trending-news', trendingNewsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
