bfd25beb0dda4ea32ae816fb99ec4acbc21c7931
Wei Seng Tan <tanweiseng13@gmail.com>
2020-01-26 20:58:34 +0800

    Update get news API endpoint to support sort, limit and offset

--- routes/news.js
+++ routes/news.js
@@ -3,22 +3,72 @@ const router = express.Router();
 const asyncHandler = require("express-async-handler");
 const db = require('../system/database');
 
-router.get('/', function(req, res, next) {
-  var conn = db.conn
-  var q = req.query.q;
-  var query = "SELECT * FROM newsapi LIMIT 50";
+/**
+ * Example sort:
+ * -publishedAt: ORDER BY publishedAt DESC
+ * +publishedAt: ORDER BY publishedAt ASC
+ * publishedAt: ORDER BY publishedAt ASC
+ */
+router.get('/', async function(req, res, next) {
+  const { limit, offset, sort, q } = req.query;
+  try {
+    const results = await getNews({ limit, offset, sort, q });
+    return res.json(results);
+  }
+  catch (error) {
+    console.log('[/news] error', error);
+    return res.json(error);
+  }
+});
+
+async function getNews({ limit = 10, offset = 0, sort , q }) {
+  limit = parseInt(limit);
+  offset = parseInt(limit);
+
+  const conn = db.conn.promise();
+
+  let query = "SELECT * FROM newsapi ";
+  let args = [];
+
   if (q) {
-    query = `SELECT * FROM newsapi WHERE description LIKE '%${q}%' LIMIT 50`
+    query += ` WHERE description LIKE %?% `;
+    args.push(q);
   }
 
-  conn.query(query, function(error, results){
-      if ( error ){
-          res.status(400).send('Error in database operation');
-      } else {
-          res.json(results);
-      }
-  });
-});
+  if (sort) {
+    let order = sort[0];
+    let field = sort.substr(1);
+    let direction = 'ASC';
+
+    if (order === '+') {
+      direction = 'ASC'
+    }
+    else if (order === '-') {
+      direction = 'DESC';
+    }
+    else {
+      direction = 'ASC'
+      field = sort;
+    }
+
+    query += ` ORDER BY ${conn.escapeId(field)} ${direction}`;
+  }
+
+  if (limit) {
+    query += ` LIMIT ? `;
+    args.push(limit);
+  }
+
+  if (offset) {
+    query += ` OFFSET ? `;
+    args.push(offset);
+  }
+
+  query += ';';
+
+  let result = await conn.query(query, args);
+  return result[0];
+}
 
 function obtainNews(page, limit) {
   let conn = db.conn;
@@ -30,6 +80,7 @@ function obtainNews(page, limit) {
       }
   });
 }
+
 /* GET home page. */
 router.get('/:page?/:limit?', asyncHandler(async function (req, res, next) {
     try {
@@ -44,7 +95,7 @@ router.get('/:page?/:limit?', asyncHandler(async function (req, res, next) {
         let query = await conn.execute("SELECT * FROM news" + limit_query, args_query);
         let result = query[0];
         res.json(result);
-        
+
     } catch (ex) {
         res.json({ status: 'error', message: ex.message });
     }
