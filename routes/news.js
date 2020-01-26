251ce05fe7999c11ebf38847ff1a20e6119d2621
Dr. Lau Cher Han <laucherhan@gmail.com>
2020-01-26 18:54:28 +0800

    added `search` function. Takes in `q` as parameter

--- routes/news.js
+++ routes/news.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql2');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
 
   var conn = mysql.createConnection({
@@ -12,9 +11,17 @@ router.get('/', function(req, res, next) {
       database : 'coronatracker'
   });
 
+  var q = req.query.q;
+  var query = "SELECT * FROM newsapi LIMIT 50";
+  if (q) {
+    query = `SELECT * FROM newsapi WHERE description LIKE '%${q}%' LIMIT 50`
+  }
+  
+  console.log(query);
+
   conn.connect();
 
-  conn.query('SELECT * FROM news', function(error, results){
+  conn.query(query, function(error, results){
       if ( error ){
           res.status(400).send('Error in database operation');
       } else {
