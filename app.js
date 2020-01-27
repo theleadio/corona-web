afdb2f1bb2563a8655c43b070096b2f3289867c9
Wei Seng Tan <tanweiseng13@gmail.com>
2020-01-27 17:25:55 +0800

    Remove country route

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var newsRouter = require('./routes/news');
-var countryRouter = require('./routes/country');
 
 var cors = require('cors')
 
@@ -28,7 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/news', newsRouter);
-app.use('/country', countryRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
