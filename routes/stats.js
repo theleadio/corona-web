f3e155ae27192ab201412fb1f0f1874aa8a0abdc
Wei Seng Tan <tanweiseng13@gmail.com>
2020-01-29 01:45:55 +0800

    Implement stats endpoint based on arcgis data source stats

--- routes/stats.js
+++ routes/stats.js
@@ -14,7 +14,7 @@ const db = require('../system/database');
 router.get('/', asyncHandler(async function (req, res, next) {
   const { country } = req.query;
   try {
-    const results = await getStats(country);
+    const results = await getStatsByArcGis(country);
     return res.json(results);
   }
   catch (error) {
@@ -23,17 +23,62 @@ router.get('/', asyncHandler(async function (req, res, next) {
   }
 }));
 
-async function getStats(country) {
+router.get('/qq', asyncHandler(async function(req, res, next) {
+  const { country } = req.query;
+  try {
+    const results = await getStatsByQq(country);
+    return res.json(results);
+  }
+  catch (error) {
+    console.log('[/stats] error', error);
+    return res.json(error);
+  }
+}));
+
+async function getStatsByArcGis(country) {
   const conn = db.conn.promise();
   let query = '';
   const args = [];
 
   if (!country) {
     query = `SELECT 
-CAST(SUM(num_confirm) AS UNSIGNED) AS num_confirm, 
+CAST(SUM(Confirmed) AS UNSIGNED) AS num_confirm, 
+CAST(SUM(Deaths) AS UNSIGNED) AS num_dead,
+CAST(SUM(Recovered) AS UNSIGNED) AS num_heal, 
+posted_date
+FROM arcgis
+GROUP BY posted_date
+ORDER BY posted_date DESC
+LIMIT 1`;
+  }
+  else {
+    query = `
+SELECT country, COALESCE(Confirmed, 0) AS num_confirm, COALESCE(Deaths, 0) AS num_dead, COALESCE(Recovered, 0) AS num_heal, posted_date
+FROM arcgis
+WHERE country LIKE ?
+ORDER BY posted_date DESC
+`;
+
+    // using like and % instead of =
+    // because some country in the database has extra space/invisible character.
+    args.push(`%${country}%`);
+  }
+
+  let result = await conn.query(query, args);
+  return result[0] && result[0][0] || { country, num_confirm: '?', num_suspect: '?', num_dead: '?', num_heal: '?', created: null };
+}
+
+async function getStatsByQq(country) {
+  const conn = db.conn.promise();
+  let query = '';
+  const args = [];
+
+  if (!country) {
+    query = `SELECT
+CAST(SUM(num_confirm) AS UNSIGNED) AS num_confirm,
 CAST(SUM(num_suspect) AS UNSIGNED) AS num_suspect,
 CAST(SUM(num_dead) AS UNSIGNED) AS num_dead,
-CAST(SUM(num_heal) AS UNSIGNED) AS num_heal, 
+CAST(SUM(num_heal) AS UNSIGNED) AS num_heal,
 created
 FROM tencent_data_by_country
 GROUP BY created
@@ -44,7 +89,7 @@ LIMIT 1`;
     query = `
 SELECT country, num_confirm, num_suspect, num_dead, num_heal, created
 FROM tencent_data_by_country
-WHERE country = ? 
+WHERE country = ?
 ORDER BY created DESC
 LIMIT 1
 `;
