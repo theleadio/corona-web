e620a419c95754a81ffcc793dd5e2853c50f3629
Wei Seng Tan <tanweiseng13@gmail.com>
2020-01-29 18:06:04 +0800

    Update sql query for retrieving stats

--- routes/stats.js
+++ routes/stats.js
@@ -48,22 +48,36 @@ async function getStatsByArcGis(country) {
   const args = [];
 
   if (!country) {
-    query = `SELECT 
-CAST(SUM(Confirmed) AS UNSIGNED) AS num_confirm, 
-CAST(SUM(Deaths) AS UNSIGNED) AS num_dead,
-CAST(SUM(Recovered) AS UNSIGNED) AS num_heal, 
-posted_date
-FROM arcgis
-GROUP BY posted_date
-ORDER BY posted_date DESC
-LIMIT 1`;
+//     query = `SELECT
+// CAST(SUM(Confirmed) AS UNSIGNED) AS num_confirm,
+// CAST(SUM(Deaths) AS UNSIGNED) AS num_dead,
+// CAST(SUM(Recovered) AS UNSIGNED) AS num_heal,
+// posted_date
+// FROM arcgis
+// GROUP BY posted_date
+// ORDER BY posted_date DESC
+// LIMIT 1`;
+
+    query = `SELECT
+CAST(SUM(agg_confirmed) AS UNSIGNED) AS num_confirm,
+CAST(SUM(agg_death) AS UNSIGNED) AS num_dead,
+CAST(SUM(agg_recover) AS UNSIGNED) AS num_heal
+FROM AGGREGATE_arcgis_country
+`;
   }
   else {
-    query = `
-SELECT country, COALESCE(Confirmed, 0) AS num_confirm, COALESCE(Deaths, 0) AS num_dead, COALESCE(Recovered, 0) AS num_heal, posted_date
-FROM arcgis
-WHERE country LIKE ?
-ORDER BY posted_date DESC
+//     query = `
+// SELECT country, COALESCE(Confirmed, 0) AS num_confirm, COALESCE(Deaths, 0) AS num_dead, COALESCE(Recovered, 0) AS num_heal, posted_date
+// FROM arcgis
+// WHERE country LIKE ?
+// ORDER BY posted_date DESC
+// `;
+
+    query = `SELECT
+SELECT agg_country, COALESCE(AGGREGATE_arcgis_country, 0) AS num_confirm, COALESCE(agg_death, 0) AS num_dead, COALESCE(agg_recover, 0) AS num_heal, agg_date
+FROM AGGREGATE_arcgis_country
+WHERE agg_country LIKE ?
+ORDER BY agg_date DESC   
 `;
 
     // using like and % instead of =
