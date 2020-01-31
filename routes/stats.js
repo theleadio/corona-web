0f88ae295f25e27c5be6a3174bcb32c2bf3e845e
Akio <rsalunga29@gmail.com>
2020-02-01 02:59:11 +0800

    Add db call function for trend overtime stats

--- routes/stats.js
+++ routes/stats.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const asyncHandler = require("express-async-handler");
 const db = require('../system/database');
-
+const moment = require('moment')
 
 /**
  * @api {get} /stats
@@ -41,6 +41,42 @@ router.get('/qq', asyncHandler(async function(req, res, next) {
     return res.json(error);
   }
 }));
+
+/**
+ * @api {get} /stats/trend
+ * @apiName  FetchStatsByTrend
+ * @apiGroup Stats
+ * 
+ * @apiParam {Date} [start_date] Required Start date
+ * @apiParam {Date} [end_date] Required end date
+ */
+router.get('/trend', asyncHandler(async function(req, res, next) {
+  const start_date = req.query.start_date
+  const end_date = req.query.end_date
+
+  // enforce date format
+  if (
+    moment(start_date, 'YYYY-MM-DD').format('YYYY-MM-DD') != start_date ||
+    moment(end_date, 'YYYY-MM-DD').format('YYYY-MM-DD') != end_date
+  ) {
+    res.json('Invalid date format. Date format should be YYYY-MM-DD')
+  }
+
+  // make sure end_date isn't less than start_date
+  if (moment(end_date).isBefore(start_date)) {
+    res.json('Invalid date')
+  }
+
+  try {
+    const results = await getStatsByTrend(start_date, end_date)
+    
+    return res.json(results)
+  } catch (error) {
+    console.log('[/stats/trend] error', error)
+
+    return res.json(error)
+  }
+}))
 
 async function getStatsByArcGis(country) {
   const conn = db.conn.promise();
@@ -111,6 +147,21 @@ LIMIT 1
 
   let result = await conn.query(query, args);
   return result[0] && result[0][0] || { country, num_confirm: '?', num_dead: '?', num_heal: '?', created: null };
+}
+
+async function getStatsByTrend(start_date, end_date) {
+  const conn = db.conn.promise()
+  let query = ''
+  const args = []
+
+  if (start_date && end_date) {
+    query = `SELECT * FROM AGGREGATE_arcgis WHERE (agg_date BETWEEN ? AND ?)`
+    args.push(start_date, end_date)
+  }
+
+  let result = await conn.query(query, args)
+
+  return result[0]
 }
 
 module.exports = router;
