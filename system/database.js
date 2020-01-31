73d6d27da703f2a3fdd82d401af63c8b3bddad38
Wei Seng Tan <tanweiseng13@gmail.com>
2020-01-31 13:50:05 +0800

    Standarise sql connection timezone to UTC

--- system/database.js
+++ system/database.js
@@ -4,6 +4,17 @@ let pool = mysql.createPool({
     user: 'corona',
     password: 'corona0106',
     database: 'coronatracker',
+
+    // https://github.com/sidorares/node-mysql2/issues/642#issuecomment-347500996
+    timezone: '+00:00', // Interpret all received timestamps as UTC. Otherwise local timezone is assumed.
+});
+
+pool.on('connection', conn => {
+    conn.query("SET time_zone='+00:00';", error => {
+        if (error){
+            throw error;
+        }
+    });
 });
 
 module.exports = {
