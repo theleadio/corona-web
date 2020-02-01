951e326a76f68bda39868f9e47ce28e39e416c0c
Akio <rsalunga29@gmail.com>
2020-02-01 20:09:45 +0800

    Separated analytics and stats backend

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var newsRouter = require('./routes/news');
 var statsRouter = require('./routes/stats');
+var analyticsRouter = require('./routes/analytics');
 var imageProxyRouter = require('./routes/imageProxy');
 
 var cors = require('cors')
@@ -30,6 +31,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/news', newsRouter);
 app.use('/stats', statsRouter);
+app.use('/analytics', analyticsRouter);
 app.use('/image-proxy', imageProxyRouter);
 app.use('/doc', express.static(__dirname + '/public'));
 
