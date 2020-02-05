a7ac4b7ce149a254912a94114eebb7607eabfc3d
Wei Seng Tan <tanweiseng13@gmail.com>
2020-02-05 22:35:50 +0800

    feat: setup health, timestamp, timestamp-cache endpoint for debugging

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const cache = require('../system/redis-cache');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/health', function(req, res, next) {
+  return res.json({ status: 'OK' });
+});
+
+router.get('/timestamp', function(req, res, next) {
+  return res.json({ timestamp: new Date().getTime() });
+});
+
+router.get('/timestamp-cache', cache.route(), function(req, res, next) {
+  return res.json({ timestamp: new Date().getTime() });
 });
 
 module.exports = router;
