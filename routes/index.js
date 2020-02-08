75e00cbb21dc0cb5ed0affd81a79d01626fb4a9c
Wei Seng Tan <tanweiseng13@gmail.com>
2020-02-08 22:06:15 +0800

    feat: implement countries API endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const cache = require('../system/redis-cache');
+const asyncHandler = require("express-async-handler");
+const db = require('../system/database');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,5 +20,13 @@ router.get('/timestamp', function(req, res, next) {
 router.get('/timestamp-cache', cache.route(), function(req, res, next) {
   return res.json({ timestamp: new Date().getTime() });
 });
+
+router.get('/countries', cache.route(), asyncHandler(async function(req, res, next) {
+  const conn = db.conn.promise();
+  let query = 'SELECT country_code, country_name, latitude + 0.0 AS latitude, longitude + 0.0 AS longitude FROM apps_countries';
+  let result = await conn.query(query);
+
+  return res.json(result[0]);
+}));
 
 module.exports = router;
