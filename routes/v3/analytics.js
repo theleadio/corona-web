27fe1e3090dc0dee42c1c99b12fbbf7d4118fce1
Samuel Lee <samueljklee@gmail.com>
2020-03-22 01:20:25 -0700

    change: remove unused codes and remove date option in /v3/analytics/country

--- routes/v3/analytics.js
+++ routes/v3/analytics.js
@@ -7,44 +7,7 @@ const router = express.Router()
 const { cacheCheck } = require('../../services/cacheMiddleware');
 
 /**
- * @api {get} /v3/analytics/trend By date
- * @apiName FetchAnalyticsTrendByDate
- * @apiGroup Analytics
- * @apiVersion 3.0.0
- *
- * @apiParam {String} start_date Start date in YYYY-MM-DD format
- * @apiParam {String} end_date End date in YYYY-MM-DD format
- */
-router.get('/trend', cache.route(), asyncHandler(async function(req, res, next) {
-  const start_date = req.query.start_date
-  const end_date = req.query.end_date
-
-  // enforce date format
-  if (
-    moment(start_date, 'YYYY-MM-DD').format('YYYY-MM-DD') != start_date ||
-    moment(end_date, 'YYYY-MM-DD').format('YYYY-MM-DD') != end_date
-  ) {
-    res.json('Invalid date format. Date format should be YYYY-MM-DD')
-  }
-
-  // make sure end_date isn't less than start_date
-  if (moment(end_date).isBefore(start_date)) {
-    res.json('Invalid date')
-  }
-
-  try {
-    const results = await fetchTrendByDate(start_date, end_date)
-    
-    return res.json(results)
-  } catch (error) {
-    console.log('[/analytics/trend] error', error)
-
-    return res.json(error)
-  }
-}))
-
-/**
- * @api {get} /v3/analytics/daily By area
+ * @api {get} /v3/analytics/daily By new daily cases and deaths
  * @apiName fetchTopCountryWithDailyNewCases
  * @apiGroup Analytics
  * @apiVersion 3.0.0
@@ -83,7 +46,6 @@ router.get('/daily', cache.route(), asyncHandler(async function(req, res, next) 
  */
 router.get('/country', cacheCheck, cache.route(), asyncHandler(async function(req, res, next) {
   let limit = 200
-  let date = null
 
   if (req.query.hasOwnProperty('limit')) {
     if (parseInt(req.query.limit)) {
@@ -93,22 +55,11 @@ router.get('/country', cacheCheck, cache.route(), asyncHandler(async function(re
     }
   }
 
-  if (req.query.hasOwnProperty('date')) {
-    date = req.query.date
-
-    // enforce date format
-    if (moment(date, 'YYYY-MM-DD').format('YYYY-MM-DD') !== date) {
-      return res.json('Invalid date format. Date format should be YYYY-MM-DD')
-    }
-  }
-
   try {
-    const results = await fetchAffectedCountries(limit, date)
-
+    const results = await fetchAffectedCountries(limit)
     return res.json(results)
   } catch (error) {
     console.log('[/analytics/country] error', error)
-
     return res.json(error)
   }
 }))
@@ -146,15 +97,12 @@ async function fetchTopCountryWithDailyNewCases(limit=10) {
   LIMIT ?`
 
   args.push(limit)
-
   let result = await conn.query(query, args)
-
   return result[0]
 }
 
-async function fetchAffectedCountries(limit = 999, date = null) {
-  const results = await getStatsWithCountryDetail(limit, date);
-  console.log(results)
+async function fetchAffectedCountries(limit = 999) {
+  const results = await getStatsWithCountryDetail(limit);
   return results.map(s => {
     return {
       countryCode: s.countryCode,
@@ -182,21 +130,12 @@ async function fetchAffectedCountries(limit = 999, date = null) {
  * @param limit
  * @returns {Promise<*>}
  */
-async function getStatsWithCountryDetail(limit = 999, date = null) {
+async function getStatsWithCountryDetail(limit = 999) {
     limit = parseInt(limit);
   
     const conn = db.conn.promise();
-    const args = [];
+    const args = [limit];
     
-    let dateQuery = '(SELECT MAX(last_updated) FROM worldometers)'
-    if (date) {
-      const dateFrom = moment(date).format('YYYY-MM-DD')
-      const dateTo = moment(date).add(1, 'day').format('YYYY-MM-DD')
-      dateQuery = `(SELECT MAX(last_updated) FROM worldometers WHERE last_updated >= ? and last_updated < ?)`
-      args.push(dateFrom, dateTo)
-    }
-    args.push(limit)
-  
     const query = `
     SELECT ac.country_code AS countryCode, ac.latitude + 0.0 AS lat, ac.longitude + 0.0 AS lng, tt.country AS countryName, tt.total_cases AS totalConfirmed, tt.total_deaths AS totalDeaths, tt.total_recovered AS totalRecovered, tt.last_updated AS lastUpdated
     FROM worldometers tt
@@ -219,11 +158,10 @@ async function getStatsWithCountryDetail(limit = 999, date = null) {
       longitude	
       FROM apps_countries
     )
-    AS ac ON tt.country = ac.country_alias 
+    AS ac ON tt.country = ac.country_alias
     GROUP BY tt.country
     ORDER BY tt.total_cases DESC
     LIMIT ?;`;
-    // AND tt.last_updated = ${dateQuery}
     const result = await conn.query(query, args)
     return result[0]
 }
