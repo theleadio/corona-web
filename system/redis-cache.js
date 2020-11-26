a37c61e52ebccf1ca885bfb7bc2fa9c15d52c476
Wei Seng Tan <tanweiseng13@gmail.com>
2020-11-26 22:34:24 +0800

    chore: allow setting redis auth password via environment variable

--- system/redis-cache.js
+++ system/redis-cache.js
@@ -1,8 +1,14 @@
-var cache = require('express-redis-cache')({
+var options = {
   host: process.env.REDIS_HOST || 'localhost',
   port: (process.env.REDIS_PORT && parseInt(process.env.REDIS_PORT)) || 6379,
   expire: (process.env.REDIS_EXPIRE && parseInt(process.env.REDIS_EXPIRE)) || 60,
-});
+};
+
+if (process.env.REDIS_AUTH_PASS) {
+  options.auth_pass = process.env.REDIS_AUTH_PASS;
+}
+
+var cache = require('express-redis-cache')(options);
 
 cache.on('error', function(error) {
   console.error('cache error:', error);
