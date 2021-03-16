6dd8c041e19302c3a83e64655974e77586b44650
Jonathan Gin <jonathangin52@gmail.com>
2021-03-16 17:18:25 -0400

    Refactor system/database.js to apply aggregate pattern

--- system/database.js
+++ system/database.js
@@ -2,61 +2,57 @@ require('dotenv').config()
 const fs = require('fs');
 const mysql = require('mysql2');
 
-const readConfig = {
-    host: process.env.DB_HOST_READ,
-    user: process.env.DB_USER_READ,
-    password: process.env.DB_PASSWORD_READ,
-    database: process.env.DB_TABLE_READ,
-
-    // https://github.com/sidorares/node-mysql2/issues/642#issuecomment-347500996
-    timezone: '+00:00', // Interpret all received timestamps as UTC. Otherwise local timezone is assumed.
-};
-
-if (process.env.DB_READ_SSL_CERT_PATH) {
-    readConfig.ssl = {
-        ca: fs.readFileSync(__dirname + process.env.DB_READ_SSL_CERT_PATH),
+const Database = (() => {
+    const baseConfig = {
+        // https://github.com/sidorares/node-mysql2/issues/642#issuecomment-347500996
+        timezone: '+00:00', // Interpret all received timestamps as UTC. Otherwise local timezone is assumed.
     };
-}
 
-const readPool = mysql.createPool(readConfig);
-
-readPool.on('connection', conn => {
-    conn.query("SET time_zone='+00:00';", error => {
-        if (error) {
-            console.log('Error connecting to read database:', error);
-            throw error;
-        }
-    });
-});
-
-const writeConfig = {
-    host: process.env.DB_HOST_WRITE,
-    user: process.env.DB_USER_WRITE,
-    password: process.env.DB_PASSWORD_WRITE,
-    database: process.env.DB_TABLE_WRITE,
-
-    // https://github.com/sidorares/node-mysql2/issues/642#issuecomment-347500996
-    timezone: '+00:00', // Interpret all received timestamps as UTC. Otherwise local timezone is assumed.
-};
-
-if (process.env.DB_WRITE_SSL_CERT_PATH) {
-    readConfig.ssl = {
-        ca: fs.readFileSync(__dirname + process.env.DB_WRITE_SSL_CERT_PATH),
+    const readConfig = {
+        ...baseConfig,
+        host: process.env.DB_HOST_READ,
+        user: process.env.DB_USER_READ,
+        password: process.env.DB_PASSWORD_READ,
+        database: process.env.DB_TABLE_READ,
     };
-}
+    const writeConfig = {
+        ...baseConfig,
+        host: process.env.DB_HOST_WRITE,
+        user: process.env.DB_USER_WRITE,
+        password: process.env.DB_PASSWORD_WRITE,
+        database: process.env.DB_TABLE_WRITE,
+    };
 
-const writePool = mysql.createPool(writeConfig);
+    if (process.env.DB_READ_SSL_CERT_PATH) {
+        readConfig.ssl = {
+            ca: fs.readFileSync(__dirname + process.env.DB_READ_SSL_CERT_PATH),
+        };
+    }
+    if (process.env.DB_WRITE_SSL_CERT_PATH) {
+        writeConfig.ssl = {
+            ca: fs.readFileSync(__dirname + process.env.DB_WRITE_SSL_CERT_PATH),
+        };
+    }
 
-writePool.on('connection', conn => {
-    conn.query("SET time_zone='+00:00';", error => {
-        if (error) {
-            console.log('Error connecting to write database:', error);
-            throw error;
-        }
-    });
-});
+    const setTimeZone = (conn, pool_name) => {
+        conn.query("SET time_zone='+00:00';", error => {
+            if (error) {
+                console.log(`Error connecting to ${pool_name} database:`, error);
+                throw error;
+            }
+        });
+    };
 
-module.exports = {
-    conn: readPool,
-    connWrite: writePool,
-}
+    const readPool = mysql.createPool(readConfig);
+    const writePool = mysql.createPool(writeConfig);
+
+    readPool.on('connection', conn => setTimeZone(conn, 'read'));
+    writePool.on('connection', conn => setTimeZone(conn, 'write'));
+
+    return {
+        conn: readPool,
+        connWrite: writePool,
+    };
+}());
+
+module.exports = Database;
