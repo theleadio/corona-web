0c9ee6bb8a6ca53c4f1c168d39f11b022a0686d5
Jonathan Gin <jonathangin52@gmail.com>
2021-04-14 20:38:02 -0400

    Apply singleton design pattern to database connector

--- system/database.js
+++ system/database.js
@@ -2,61 +2,72 @@ require('dotenv').config()
 const fs = require('fs');
 const mysql = require('mysql2');
 
-const readConfig = {
-    host: process.env.DB_HOST_READ,
-    user: process.env.DB_USER_READ,
-    password: process.env.DB_PASSWORD_READ,
-    database: process.env.DB_TABLE_READ,
+class DatabaseConnector {
+    conn; // Read pool
+    connWrite; // Write pool
 
-    // https://github.com/sidorares/node-mysql2/issues/642#issuecomment-347500996
-    timezone: '+00:00', // Interpret all received timestamps as UTC. Otherwise local timezone is assumed.
-};
-
-if (process.env.DB_READ_SSL_CERT_PATH) {
-    readConfig.ssl = {
-        ca: fs.readFileSync(__dirname + process.env.DB_READ_SSL_CERT_PATH),
-    };
-}
-
-const readPool = mysql.createPool(readConfig);
-
-readPool.on('connection', conn => {
-    conn.query("SET time_zone='+00:00';", error => {
-        if (error) {
-            console.log('Error connecting to read database:', error);
-            throw error;
+    constructor() {
+        // Ensure there only ever exists a single instance of DatabaseConnector
+        if (DatabaseConnector._instance) {
+            return DatabaseConnector._instance;
         }
-    });
-});
 
-const writeConfig = {
-    host: process.env.DB_HOST_WRITE,
-    user: process.env.DB_USER_WRITE,
-    password: process.env.DB_PASSWORD_WRITE,
-    database: process.env.DB_TABLE_WRITE,
+        const baseConfig = {
+            // https://github.com/sidorares/node-mysql2/issues/642#issuecomment-347500996
+            timezone: '+00:00', // Interpret all received timestamps as UTC. Otherwise local timezone is assumed.
+        };
+        const readConfig = {
+            ...baseConfig,
+            host: process.env.DB_HOST_READ,
+            user: process.env.DB_USER_READ,
+            password: process.env.DB_PASSWORD_READ,
+            database: process.env.DB_TABLE_READ,
+        };
+        const writeConfig = {
+            ...baseConfig,
+            host: process.env.DB_HOST_WRITE,
+            user: process.env.DB_USER_WRITE,
+            password: process.env.DB_PASSWORD_WRITE,
+            database: process.env.DB_TABLE_WRITE,
+        };
 
-    // https://github.com/sidorares/node-mysql2/issues/642#issuecomment-347500996
-    timezone: '+00:00', // Interpret all received timestamps as UTC. Otherwise local timezone is assumed.
-};
-
-if (process.env.DB_WRITE_SSL_CERT_PATH) {
-    readConfig.ssl = {
-        ca: fs.readFileSync(__dirname + process.env.DB_WRITE_SSL_CERT_PATH),
-    };
-}
-
-const writePool = mysql.createPool(writeConfig);
-
-writePool.on('connection', conn => {
-    conn.query("SET time_zone='+00:00';", error => {
-        if (error) {
-            console.log('Error connecting to write database:', error);
-            throw error;
+        if (process.env.DB_READ_SSL_CERT_PATH) {
+            readConfig.ssl = {
+                ca: fs.readFileSync(__dirname + process.env.DB_READ_SSL_CERT_PATH),
+            };
         }
-    });
-});
+        if (process.env.DB_WRITE_SSL_CERT_PATH) {
+            writeConfig.ssl = {
+                ca: fs.readFileSync(__dirname + process.env.DB_WRITE_SSL_CERT_PATH),
+            };
+        }
 
-module.exports = {
-    conn: readPool,
-    connWrite: writePool,
+        this.readPool = mysql.createPool(readConfig);
+        this.writePool = mysql.createPool(writeConfig);
+
+        this.readPool.on('connection', conn => this._setTimeZone(conn, 'read'));
+        this.writePool.on('connection', conn => this._setTimeZone(conn, 'write'));
+
+        DatabaseConnector._instance = this;
+        return DatabaseConnector._instance;
+    }
+
+    get conn() {
+        return this.readPool;
+    }
+
+    get connWrite() {
+        return this.writePool;
+    }
+
+    _setTimeZone(conn, poolName) => {
+        conn.query("SET time_zone='+00:00';", error => {
+            if (error) {
+                console.log(`Error connecting to ${pool_name} database:`, error);
+                throw error;
+            }
+        });
+    }
 }
+
+module.exports = new DatabaseConnector();
