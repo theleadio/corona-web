4302c5a4852f97080283f681f1ffda3f6a002b8e
Wei Seng Tan <tanweiseng13@gmail.com>
2022-06-14 13:05:21 +0800

    feat: configure cors origin

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: ["https://coronatracker.com", /\.coronatracker\.com$/] }));
 
 //rate limiting
 const rateLimitTimeMinutes = process.env.RATE_LIMIT_TIME_MINUTES;
